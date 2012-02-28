34f74c9a4a5db8c948847f36ef7573ecff1f10a1
Gaël PORT <gael.port@rbs.fr>
2012-02-28 16:29:52 +0100

    [FIX] Hovers on iPhone and iPad.

--- js/common.js
+++ js/common.js
@@ -1,15 +1,7 @@
 // Alternative to hover for devices that don't handle it (like iPhone and iPad).
 jQuery(document).ready(function() {
-	jQuery('.modules-website-switchlanguage .scroll').click(function() {
-		jQuery(this).toggleClass('show');
-	});
-	jQuery('.modules-order-ShortCart .scroll').click(function() {
-		jQuery(this).toggleClass('show');
-	});
-	jQuery('.modules-customer-Customerinfo .scroll').click(function() {
-		jQuery(this).toggleClass('show');
-	});
-	jQuery('.modules-users-showinfo .scroll').click(function() {
-		jQuery(this).toggleClass('show');
-	});
+	jQuery('.modules-website-switchlanguage .scroll').click(function() {});
+	jQuery('.modules-order-ShortCart .scroll').click(function() {});
+	jQuery('.modules-customer-Customerinfo .scroll').click(function() {});
+	jQuery('.modules-users-showinfo .scroll').click(function() {});
 });
